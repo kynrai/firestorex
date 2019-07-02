d5c4b96c54de0be2e573bd509aa53817372d882f
Steven Lee <kynrai@gmail.com>
2019-07-02 15:16:56 +0100

    Initial commit

---
+++ firestorex.go
@@ -0,0 +1,72 @@
+package firestorex
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"cloud.google.com/go/firestore"
+	"golang.org/x/sync/errgroup"
+)
+
+type settings struct {
+	maxConcurrecy  int
+	batchChunkSize int
+}
+
+type Option func(*settings)
+
+func defaultSettings() *settings {
+	s := new(settings)
+	s.maxConcurrecy = 10
+	s.batchChunkSize = 500
+	return s
+}
+
+func MaxConcurrency(max int) Option {
+	return func(s *settings) {
+		s.maxConcurrecy = max
+	}
+}
+
+func BatchChunkSize(chunkSize int) Option {
+	return func(s *settings) {
+		s.batchChunkSize = chunkSize
+	}
+}
+
+func BatchWrite(ctx context.Context, client *firestore.Client, collection string, v interface{}, opts ...Option) error {
+	s := defaultSettings()
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	data := reflect.ValueOf(v)
+	if data.Kind() != reflect.Slice {
+		return fmt.Errorf("v is expected to be a slice")
+	}
+	g, ctx := errgroup.WithContext(ctx)
+
+	batches := func(ctx context.Context, client *firestore.Client, v reflect.Value) error {
+		throttle := make(chan struct{}, s.maxConcurrecy)
+		max := v.Len()
+		for i := 0; i < max; i += s.batchChunkSize {
+			end := i + s.batchChunkSize
+			if end > max {
+				end = max
+			}
+			d := v.Slice(i, end)
+			g.Go(func() error {
+				batch := client.Batch()
+				for i := 0; i < d.Len(); i++ {
+					d.Index(i).Interface()
+				}
+				return nil
+			})
+		}
+
+		return g.Wait()
+	}
+
+	return batches(ctx, client, data)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kynrai/firestorex
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.41.0
+	github.com/google/uuid v1.1.1
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+)
